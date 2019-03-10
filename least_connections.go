fd17482384b5264053f75718005be36b015007a8
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-10 23:04:38 +0900

    Tweak

--- least_connections.go
+++ least_connections.go
@@ -1,34 +1,24 @@
 package leastconnections
 
 import (
-	"log"
 	"net/url"
 	"sync"
 
 	"github.com/pkg/errors"
-
-	"github.com/hlts2/lock-free"
 )
 
 // ErrServersNotExist is the error that servers dose not exists
 var ErrServersNotExist = errors.New("servers dose not exist")
 
-// ErrServerNotExist is the error that server dose not exists
-var ErrServerNotExist = errors.New("server dose not exist")
-
+// LeastConnections --
 type LeastConnections interface {
-	IncrementConnections(server string)
-	DecrementConnections(server string)
-	Next() string
+	Next() (next *url.URL, done func())
 }
 
 type leastConnections struct {
-	servers     []string
-	connections map[string]int
-	lf          lockfree.LockFree
-
-	urls []*url.URL
-	mu   *sync.Mutex
+	urls  []*url.URL
+	conns map[int]int
+	mu    *sync.Mutex
 }
 
 // New initializes a new instance of LeastConnected
@@ -37,65 +27,40 @@ func New(urls []*url.URL) (LeastConnections, error) {
 		return nil, ErrServersNotExist
 	}
 
-	connections := make(map[string]int)
-	for _, url := range urls {
-		connections[url.Scheme+"://"+url.Host] = 0
+	conns := make(map[int]int)
+	for i := range urls {
+		conns[i] = 0
 	}
 
 	return &leastConnections{
-		urls:        urls,
-		connections: connections,
-		lf:          lockfree.New(),
-		mu:          new(sync.Mutex),
+		urls:  urls,
+		conns: conns,
+		mu:    new(sync.Mutex),
 	}, nil
 }
 
-// IncrementConnection increments the number of connection of server
-func (lc *leastConnections) IncrementConnections(server string) {
-	lc.lf.Wait()
-
-	if _, ok := lc.connections[server]; ok {
-		lc.connections[server]++
-	}
-
-	lc.lf.Signal()
-}
-
-// DecrementConnection decrements the number of connection of server
-func (lc *leastConnections) DecrementConnections(server string) {
-	lc.lf.Wait()
-
-	if v, ok := lc.connections[server]; ok {
-		if v > 0 {
-			lc.connections[server]--
-		}
-	}
-
-	lc.lf.Signal()
-}
-
-func (lc *leastConnections) Next() string {
-	lc.lf.Wait()
-
-	var minConnectionsServer string
-	minConnections := -1
-
+func (lc *leastConnections) Next() (*url.URL, func()) {
 	var (
-		cnt int
-		ok  bool
+		min = -1
+		idx int
 	)
-	for _, server := range lc.servers {
-		if cnt, ok = lc.connections[server]; !ok {
-			log.Fatal(errors.WithMessage(ErrServerNotExist, server))
-		}
 
-		if minConnections == -1 || cnt < minConnections {
-			minConnections = cnt
-			minConnectionsServer = server
+	lc.mu.Lock()
+
+	for urlIdx, cnt := range lc.conns {
+		if min == -1 || cnt < min {
+			min = cnt
+			idx = urlIdx
 		}
 	}
 
-	lc.lf.Signal()
+	lc.conns[idx]++
 
-	return minConnectionsServer
+	lc.mu.Unlock()
+
+	return lc.urls[idx], func() {
+		lc.mu.Lock()
+		lc.conns[idx]--
+		lc.mu.Unlock()
+	}
 }
